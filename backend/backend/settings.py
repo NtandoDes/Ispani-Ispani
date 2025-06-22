aa8129216bfde995d642de85e909a0bb7e0d0f8e
NtandoDes <134165057+NtandoDes@users.noreply.github.com>
2025-06-22 20:57:18 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -216,11 +216,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ASGI_APPLICATION = "backend.asgi.application"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # FIXED: Channel Layers Configuration
 # Use InMemoryChannelLayer for development to avoid Redis version compatibility issues
